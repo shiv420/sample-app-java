f11b313cfdf6a98c83e22ddd8a1be062925c5f9d
shiv420 <shiv.goods@gmail.com>
2024-11-09 16:21:21 +0530

    Commit Added

--- src/main/java/com/devopsdemo/helper/GenericResourceBundlev2.java
+++ src/main/java/com/devopsdemo/helper/GenericResourceBundlev2.java
@@ -8,6 +8,7 @@ public class GenericResourceBundlev2 {
 		ResourceBundle rb = ResourceBundle.getBundle("ResourceBundle");
 		Enumeration <String> keys = rb.getKeys();
 		String value="";
+		System.out.println("Git push and trigger the build");
 		while (keys.hasMoreElements()) {
 			
 			String key =  keys.nextElement();
